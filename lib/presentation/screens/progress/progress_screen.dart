f86f103835898e51e6cf56be9a57787b3539ac3e
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-15 12:36:28 -0400

    Add examples for progress indicators

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -31,7 +31,7 @@ class _ProgressView extends StatelessWidget {
           ),
 
           SizedBox(height: 20),
-          Text('Progress indicator controlado'),
+          Text('Circular y Linear controlado'),
           SizedBox(height: 10),
           _ControlledProgressIndicator(),
         ],
@@ -45,6 +45,29 @@ class _ControlledProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+        return (value * 2) / 100; // 0.0, 1.0
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+        final progressValue = snapshot.data ?? 0;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: progressValue,
+                strokeWidth: 2,
+                backgroundColor: Colors.black12,
+              ),
+              const SizedBox(width: 20),
+              Expanded(child: LinearProgressIndicator(value: progressValue)),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
