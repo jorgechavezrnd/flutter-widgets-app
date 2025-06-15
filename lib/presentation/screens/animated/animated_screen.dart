576d6853855016ae70f9912a5e01f3e6ecfef276
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-15 14:58:37 -0400

    Finish example for AnimatedContainer

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,9 +1,37 @@
+import 'dart:math' show Random;
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double height = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changeShape() {
+    final random = Random();
+
+    width = random.nextInt(300) + 120;
+    height = random.nextInt(300) + 120;
+    borderRadius = random.nextInt(100) + 20;
+
+    color = Color.fromRGBO(
+      random.nextInt(255), // red
+      random.nextInt(255), // green
+      random.nextInt(255), // blue
+      1, // opacity
+    );
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +41,20 @@ class AnimatedScreen extends StatelessWidget {
       body: Center(
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 400),
-          curve: Curves.easeOutCubic,
-          width: 300,
-          height: 230,
+          curve: Curves.elasticOut,
+          width: width <= 0 ? 0 : width,
+          height: height <= 0 ? 0 : height,
           decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(30),
+            color: color,
+            borderRadius: BorderRadius.circular(
+              borderRadius <= 0 ? 0 : borderRadius,
+            ),
           ),
         ),
       ),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: changeShape,
         child: const Icon(Icons.play_arrow_rounded),
       ),
     );
