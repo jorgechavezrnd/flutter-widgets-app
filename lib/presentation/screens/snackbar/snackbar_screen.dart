485d8e0c22a938157d76a3aa0ddcaec8b00a1aa3
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-15 14:37:55 -0400

    Add dialags examples

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -17,10 +18,60 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estás seguro?'),
+        content: const Text(
+          'Veniam magna occaecat dolore minim est. In nulla veniam exercitation exercitation do. Cillum excepteur officia cupidatat tempor aliqua irure minim in aliquip veniam nulla adipisicing nulla. Nisi ipsum commodo officia laboris non in reprehenderit eu non eiusmod. Sint magna exercitation est ad commodo excepteur adipisicing excepteur fugiat.',
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: const Text('Cancelar'),
+          ),
+
+          FilledButton(
+            onPressed: () => context.pop(),
+            child: const Text('Aceptar'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Snackbars y Diálogos')),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text(
+                      'Ex officia sint laboris nulla. Ea do ullamco est labore minim adipisicing esse non nisi esse nisi dolore esse dolor. Ex fugiat elit voluptate nostrud non veniam aliquip eu tempor. Laborum ut qui esse reprehenderit incididunt fugiat irure. Labore incididunt deserunt reprehenderit consequat consequat exercitation. Sint incididunt adipisicing consectetur fugiat veniam ipsum dolor aute veniam duis.',
+                    ),
+                  ],
+                );
+              },
+              child: const Text('Licencias usadas'),
+            ),
+
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Mostrar diálogo'),
+            ),
+          ],
+        ),
+      ),
 
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar Snackbar'),
