67106a71debb2962399389ded6adcbbe8618900f
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-15 23:43:54 -0400

    Add logic for build options based on Menu Items

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({super.key});
@@ -12,6 +13,8 @@ class _SideMenuState extends State<SideMenu> {
 
   @override
   Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
       selectedIndex: navDrawerIndex,
       onDestinationSelected: (value) {
@@ -20,15 +23,38 @@ class _SideMenuState extends State<SideMenu> {
         });
       },
       children: [
-        NavigationDrawerDestination(
-          icon: const Icon(Icons.add),
-          label: const Text('Home Screen'),
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20, 16, 10),
+          child: const Text('Main'),
         ),
 
-        NavigationDrawerDestination(
-          icon: const Icon(Icons.add_shopping_cart_rounded),
-          label: const Text('Otra pantalla'),
+        ...appMenuItems
+            .sublist(0, 3)
+            .map(
+              (item) => NavigationDrawerDestination(
+                icon: Icon(item.icon),
+                label: Text(item.title),
+              ),
+            ),
+
+        Padding(
+          padding: const EdgeInsets.fromLTRB(28, 16, 28, 10),
+          child: Divider(),
         ),
+
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+          child: Text('More options'),
+        ),
+
+        ...appMenuItems
+            .sublist(3)
+            .map(
+              (item) => NavigationDrawerDestination(
+                icon: Icon(item.icon),
+                label: Text(item.title),
+              ),
+            ),
       ],
     );
   }
